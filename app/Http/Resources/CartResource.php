a66d0940a1f6e4ce9612db8e113d9aeb5663fada
Exauce Bodi <exaucebodi4@gmail.com>
2025-05-09 17:43:57 +0200

    Ajout des resource pour Cart et CartItem

--- app/Http/Resources/CartResource.php
+++ app/Http/Resources/CartResource.php
@@ -14,6 +14,14 @@ class CartResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'user_id' => $this->user_id,
+            'total_price' => $this->total_price,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+            'cart_items' => CartItemResource::collection($this->whenLoaded('cartItems')),
+            'user' => new UserResource($this->whenLoaded('user')),
+        ];
     }
 }
